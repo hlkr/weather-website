1c3a1a9880d1c1334606ee537e41b3b9d68b61e0
Luis <fa_ntasma.ze-ro@outlook.es>
2020-10-07 20:05:38 -0500

    Add humidity

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,7 @@ const forecast = (latitude, longitude, callback)=>{
         }else if(body.error){
             callback('Unable to find location', undefined);
         }else{
-            callback(undefined, `${body.current.weather_descriptions[0]}. The temperature is ${body.current.temperature}°F out. And it feels like ${body.current.feelslike}°F`);
+            callback(undefined, `${body.current.weather_descriptions[0]}. The temperature is ${body.current.temperature}°F out. And it feels like ${body.current.feelslike}°F and the humidity is: ${body.current.humidity}%`);
         };
     });
 }
